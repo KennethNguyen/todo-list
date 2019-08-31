bf713499ebe10c3959a263b398bcebad923b6150
Kenneth Nguyen <n.kennethcs@gmail.com>
2019-08-30 19:08:30 -0700

    Create initial todo-list; App.js takes data from todosData.js and renders it with functional checkboxes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from './components/TodoItem'
+import todosData from './todosData'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo 
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+
+    render() {
+        const todoComponent = this.state.todos.map(todo => <TodoItem key={todo.id} task={todo} handleChange={this.handleChange} />)
+        return (
+            <div className="todo-list">
+                {todoComponent}
+            </div>
+        )    
+    }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoItem = props => {
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.task.completed}
+                onChange={() => props.handleChange(props.task.id)} 
+            />
+            <p>{props.task.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem;
