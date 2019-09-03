408dc416ad02d466c8971d823f6d0a8447331a33
Kenneth Nguyen <n.kennethcs@gmail.com>
2019-09-03 16:29:05 -0700

    Updates

---
+++ precache-manifest.b7d34ddb42642a4a358962e6c5c811a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e9e7557a8092bc62097aaab4e11c8f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "4a7f1c49f065159b4ba5",
+    "url": "/todo-list/static/css/main.7409cad9.chunk.css"
+  },
+  {
+    "revision": "e2a9bd1f0358ecd91369",
+    "url": "/todo-list/static/js/2.cfb935b7.chunk.js"
+  },
+  {
+    "revision": "4a7f1c49f065159b4ba5",
+    "url": "/todo-list/static/js/main.c65d36d4.chunk.js"
+  },
+  {
+    "revision": "af34df7a0b7b4cbd00aa",
+    "url": "/todo-list/static/js/runtime~main.40be76a2.js"
+  }
+]);
